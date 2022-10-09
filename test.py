3041866ecba589b39694720b38ef380182ddb2ee
Jerome <jerome.siljan@mavs.uta.edu>
2022-10-09 16:01:59 -0500

    reading cartoons

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ from torch.autograd import Variable
 from matplotlib import pyplot as plt
 from matplotlib import image as img
 from matplotlib.patches import Rectangle
+import pytesseract
 
 from PIL import Image
 
@@ -29,6 +30,7 @@ import json
 import zipfile
 
 from craft import CRAFT
+
 
 from collections import OrderedDict
 def copyStateDict(state_dict):
@@ -68,42 +70,6 @@ image_list, _, _ = file_utils.get_files(args.test_folder)
 result_folder = './result/'
 if not os.path.isdir(result_folder):
     os.mkdir(result_folder)
-
-def show_graph(lined_boxes):
-    for box in lined_boxes:
-        if(box[0] == 0):
-            box_coords = box[1]
-            plt.scatter(box_coords[0], box_coords[1], c='red')
-            plt.scatter(box_coords[2], box_coords[3], c='red')
-            plt.imshow(test_img)
-        elif(box[0] == 1):
-            box_coords = box[1]
-            plt.scatter(box_coords[0], box_coords[1], c='green')
-            plt.scatter(box_coords[2], box_coords[3], c='green')
-            plt.imshow(test_img)
-        elif(box[0] == 2):
-            box_coords = box[1]
-            plt.scatter(box_coords[0], box_coords[1], c='blue')
-            plt.scatter(box_coords[2], box_coords[3], c='blue')
-            plt.imshow(test_img)
-        elif(box[0] == 3):
-            box_coords = box[1]
-            plt.scatter(box_coords[0], box_coords[1], c='purple')
-            plt.scatter(box_coords[2], box_coords[3], c='purple')
-            plt.imshow(test_img)
-        elif(box[0] == 4):
-            box_coords = box[1]
-            plt.scatter(box_coords[0], box_coords[1], c='yellow')
-            plt.scatter(box_coords[2], box_coords[3], c='yellow')
-            plt.imshow(test_img)
-        elif(box[0] == 5):
-            box_coords = box[1]
-            plt.scatter(box_coords[0], box_coords[1], c='orange')
-            plt.scatter(box_coords[2], box_coords[3], c='orange')
-            plt.imshow(test_img)
-
-    plt.savefig(f'result/res_{image_path[16:-4]}_sep{image_path[-4:]}')
-    plt.clf()
 
 def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net=None):
     t0 = time.time()
@@ -159,11 +125,56 @@ def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, r
 
     return boxes, polys, ret_score_text
 
+def read_img(image_path):
+    thresh_const = 4
+    threshold = 0
+    prev_endY = 0
+    lined_boxes = []
+    lined_boxes_index = 0
+
+    print('Test image {image_path}')
+    image = imgproc.loadImage(image_path)
+    test_img = img.imread(image_path)
+
+    bboxes, polys, score_text = test_net(net, image, args.text_threshold, args.link_threshold, args.low_text, args.cuda, args.poly, refine_net)
+
+    # boxes has form [[startX, startY, endX, endY]]
+    unsorted_boxes = [[int(box[0][0]), int(box[0][1]), int(box[1][0]), int(box[2][1])] for box in bboxes]
+    boxes = sorted(unsorted_boxes, key=lambda x: x[1])
+
+    for index, box in enumerate(boxes):
+        if(index == 0):
+            threshold = thresh_const + abs(box[1] - box[3])
+        elif(abs(prev_endY - box[1]) > threshold):
+            lined_boxes_index += 1
+
+        lined_boxes.append([lined_boxes_index, box])
+        prev_endY = box[1]
+    print()
+    max_para = max([box[0] for box in lined_boxes])
+
+    plt.imshow(test_img)
+    message = []
+    for x in range(max_para + 1):
+        para  = [box[1] for box in lined_boxes if box[0] == x]
+        min_startX = min([val[0] for val in para])
+        min_startY = min([val[1] for val in para])
+        max_endX = max([val[2] for val in para])
+        max_endY = max([val[3] for val in para])
+
+        orig = cv2.imread(image_path)
+        crop = orig[min_startY:max_endY, min_startX:max_endX]
+
+        ocr_string = pytesseract.image_to_string(crop)
+        message.append(ocr_string)
+
+    print(f'{image_path} message: {message}')
+
 if __name__ == '__main__':
     # load net
     net = CRAFT()     # initialize
 
-    print('Loading weights from checkpoint (' + args.trained_model + ')')
+    #print('Loading weights from checkpoint (' + args.trained_model + ')')
     if args.cuda:
         net.load_state_dict(copyStateDict(torch.load(args.trained_model)))
     else:
@@ -194,73 +205,4 @@ if __name__ == '__main__':
 
     t = time.time()
 
-
-    # load data
-    for k, image_path in enumerate(image_list):
-        thresh_const = 4
-        threshold = 0
-        prev_endY = 0
-        lined_boxes = []
-        lined_boxes_index = 0
-
-        print("Test image {:d}/{:d}: {:s}".format(k+1, len(image_list), image_path), end='\r')
-        image = imgproc.loadImage(image_path)
-        test_img = img.imread(image_path)
-
-        bboxes, polys, score_text = test_net(net, image, args.text_threshold, args.link_threshold, args.low_text, args.cuda, args.poly, refine_net)
-
-        # boxes has form [[startX, startY, endX, endY]]
-        unsorted_boxes = [[int(box[0][0]), int(box[0][1]), int(box[1][0]), int(box[2][1])] for box in bboxes]
-        boxes = sorted(unsorted_boxes, key=lambda x: x[1])
-        print()
-
-        for index, box in enumerate(boxes):
-            if(index == 0):
-                threshold = thresh_const + abs(box[1] - box[3])
-            elif(abs(prev_endY - box[1]) > threshold):
-                lined_boxes_index += 1
-
-            lined_boxes.append([lined_boxes_index, box])
-            prev_endY = box[1]
-        print()
-        max_para = max([box[0] for box in lined_boxes])
-
-        plt.imshow(test_img)
-        rects = []
-        for x in range(max_para + 1):
-            para  = [box[1] for box in lined_boxes if box[0] == x]
-            min_startX = min([val[0] for val in para])
-            min_startY = min([val[1] for val in para])
-            max_endX = max([val[2] for val in para])
-            max_endY = max([val[3] for val in para])
-            #print(f'startX: {min_startX}  startY: {min_startY}')
-            #print(f'endX: {max_endX}    endY: {max_endY}')
-            rects.append(para)
-            plt.scatter(min_startX, min_startY)
-            plt.scatter(max_endX, max_endY)
-
-            orig = cv2.imread(image_path)
-            crop = orig[min_startY:max_endY, min_startX:max_endX]
-            cv2.imshow('crop', crop)
-            cv2.waitKey()
-
-        #plt.show()
-        break
-
-        #    plt.scatter(ext_startX, ext_startY)
-        #    plt.scatter(ext_endX, ext_endY)
-        #    plt.show()
-        #    plt.clear()
-    
-
-
-        #show_graph(lined_boxes)
-
-        # save score text
-        #filename, file_ext = os.path.splitext(os.path.basename(image_path))
-        #mask_file = result_folder + "/res_" + filename + '_mask.jpg'
-        #cv2.imwrite(mask_file, score_text)
-
-        #file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
-
-    #print("elapsed time : {}s".format(time.time() - t))
+    read_img('training-strips/cartoon2.PNG')
