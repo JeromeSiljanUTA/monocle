6efe9a0cae7e32332223112ba7fe07423772b525
Chris <chrisanand@tamu.edu>
2022-10-09 16:43:04 -0500

    trying file upload

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from werkzeug import secure_filename
+import os
 
 # def startup():
 app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLD = '/Users/blabla/Desktop/kenetelli/htmlfi'
+UPLOAD_FOLDER = os.path.join(APP_ROOT, UPLOAD_FOLD)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
@@ -16,7 +22,7 @@ def upload_file():
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
+        f.save(secure_filename(os.path.join(app.config['UPLOAD_FOLDER'], f.filename)))
         return 'file uploaded successfully'
 
 if __name__ == '__main__':
