691bc3dcbefa486a5ca3d6a19a625c5a16744fd5
Chris <chrisanand@tamu.edu>
2022-10-09 14:53:29 -0500

    update

---
+++ reading.py
@@ -0,0 +1,23 @@
+import pytesseract
+import cv2
+import numpy as np
+
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+
+# list_imgs = ['training-strips/cartoon1.png'] #Take in input most likely
+# transcript = []
+# for fold in list_imgs:
+#     img = cv2.imread(fold)
+#     img = cv2.resize(img, (600,360))
+#     transcript.extend(pytesseract.image_to_string(img))
+
+# print(transcript)
+
+img = cv2.imread('training-strips/cartoon1.png')
+img = cv2.resize(img, (600, 360))
+kernel = np.ones((1, 1), np.uint8)
+img = cv2.dilate(img, kernel, iterations=1)
+img = cv2.erode(img, kernel, iterations=1)
+
+print(pytesseract.image_to_string(img))
+cv2.imshow('Result', img)
