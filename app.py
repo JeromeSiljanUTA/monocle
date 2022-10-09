1815b81b4a67ed0729c5e06e78acc42e28ea35d4
Chris <chrisanand@tamu.edu>
2022-10-09 16:35:16 -0500

    working on file upload

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask
+from werkzeug import secure_filename
 
-def startup():
-    app = Flask(__name__)
+# def startup():
+app = Flask(__name__)
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello World!'
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
-    if __name__ == '__main__':
-        app.run(port=5001)
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
 
-startup()
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
+
+if __name__ == '__main__':
+    app.run(debug = True, port=5001)
+
+# startup()
