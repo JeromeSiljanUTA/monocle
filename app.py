14162a27b6f13ea3984565eb14a6befd6b1d26a4
Jerome <jerome.siljan@mavs.uta.edu>
2022-10-10 15:12:54 -0500

    shows image on webpage

--- app.py
+++ app.py
@@ -3,24 +3,19 @@ from werkzeug.utils import secure_filename
 import cv2
 app = Flask(__name__)
 
-upload_location = 'uploads/'
+upload_location = 'static/'
 
 @app.route('/upload')
 def upload_file():
-   return render_template('upload.html')
+    return render_template('upload.html', img='')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def uploader():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(f'{upload_location}{secure_filename(f.filename)}')
-      img_path = f'{upload_location}{secure_filename(f.filename)}'
-      print(img_path)
-      img = cv2.imread(img_path)
-      cv2.imshow('uploaded image:', img)
-      cv2.waitKey(0)
-      cv2.destroyAllWindows()
-      return 'file uploaded successfully'
+    if request.method == 'POST':
+        f = request.files['file']
+        img_path = f'{upload_location}{secure_filename(f.filename)}'
+        f.save(img_path)
+        return render_template('upload.html', img=img_path)
 
 if __name__ == '__main__':
     app.run(debug = True, port=5001)
