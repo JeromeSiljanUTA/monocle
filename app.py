a28c50365d12482ededbd3402668977e842af343
Jerome <jerome.siljan@mavs.uta.edu>
2022-10-10 15:17:35 -0500

    simplified code

--- app.py
+++ app.py
@@ -5,17 +5,15 @@ app = Flask(__name__)
 
 upload_location = 'static/'
 
-@app.route('/upload')
-def upload_file():
-    return render_template('upload.html', img='')
-
-@app.route('/uploader', methods = ['GET', 'POST'])
-def uploader():
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload():
     if request.method == 'POST':
         f = request.files['file']
         img_path = f'{upload_location}{secure_filename(f.filename)}'
         f.save(img_path)
         return render_template('upload.html', img=img_path)
+    else:
+        return render_template('upload.html', img='')
 
 if __name__ == '__main__':
     app.run(debug = True, port=5001)
